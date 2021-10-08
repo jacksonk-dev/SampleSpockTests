65f69b443fe2016376d73b13e5a4f0cdae19ce4e
Power Mukisa <powermukisa@gmail.com>
2021-10-08 00:27:52 -0700

    initial commit

---
+++ loans-acceptance-tests/README.md
@@ -0,0 +1,3 @@
+to run tests and output Spock reports, in the root dir, run `./gradlew l`
+
+The reports will be located under loans-acceptance-tests/build/spock-reports
---
+++ loans-acceptance-tests/src/test/groovy/co/tala/acceptance/loans/specs/TestSpec.groovy
@@ -0,0 +1,20 @@
+package co.tala.acceptance.loans.specs
+
+
+import spock.lang.Specification
+import spock.lang.Title
+import spock.lang.Unroll
+
+@Unroll
+@Title("User does sth")
+class TestSpec extends Specification {
+    def "test two"() {
+
+        given: "accc"
+
+        when: "www"
+
+        then: "ttt"
+            1 == 1
+    }
+}
